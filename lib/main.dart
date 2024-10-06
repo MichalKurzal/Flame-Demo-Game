e9ae637b7b6d8c845c70cd4fccd11917b3a2a65d
Michal Kurzal <michalkurzal200@gmail.com>
2024-10-06 16:07:31 +0200

    Locked landscape mode for mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:demo_flame_game/demo_flame_game.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
@@ -5,6 +7,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid || Platform.isIOS) {
+    Flame.device.setLandscape();
+  }
   Flame.device.fullScreen();
   DemoFlameGame demoGame = DemoFlameGame();
 
